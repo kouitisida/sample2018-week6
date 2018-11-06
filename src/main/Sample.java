14b4211129a3588936a881358bab4d66c1f4c15e
Kouich Sida <e185705@ie.u-ryukyu.ac.jp>
2018-11-06 12:28:04 +0900

    1st commit

---
+++ src/main/Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+    public int add(int a,int b){
+        int result = a + b;
+        return result;
+    }
+}
+
+
---
+++ src/test/SampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        Sample sample = new Sample();
+        int result = sample.add(2,3);
+        assertEquals(5,result);
+
+
+
+
+
+
+    }
+}
